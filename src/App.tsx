49701fb2823bc314a55fa9f1f9f4cf6ea11615b2
Wes Warriner <warriwes@uw.edu>
2023-08-22 12:29:31 -0600

    update App and main with initial styles and first Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+    </>
   )
 }
 
