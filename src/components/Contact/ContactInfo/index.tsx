fc6f8d0c5f375b8d626e40a4384d949b0812bb7f
Wes Warriner <fridgehistory@gmail.com>
2023-08-29 20:02:50 -0600

    begin content of ContactInfo card

--- src/components/Contact/ContactInfo/index.tsx
+++ src/components/Contact/ContactInfo/index.tsx
@@ -1,10 +1,20 @@
+import SocialLinks from "../../SocialLinks";
+
 const ContactInfo = () => {
     return (
         <div>
             <h2>Get in touch</h2>
-            <li>Phone: yeah right</li>
-            <li>Email: but you can just send a message here</li>
-            <li>Address: just guess it</li>
+            <div>
+                <img aria-label="self-portrait"></img>
+                <p>I am a super-curious, easily-inspired developer. I have a lot to learn and I love that.</p>
+                <p>If you value growth opportunitues and open communication, feel free to reach out.</p>
+            </div>
+            <ul>
+                <li>Phone: 1-800-867-5309</li>
+                <li>Email: but you can just send a message here..</li>
+                <li>Fax: what is a fax machine anyway?</li>
+            </ul>
+            <SocialLinks />
         </div>
     )
 }
