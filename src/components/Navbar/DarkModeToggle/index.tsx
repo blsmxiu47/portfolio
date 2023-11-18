820a890a404e71aafc1730f91b9383a24bb5555d
Wes Warriner <fridgehistory@gmail.com>
2023-11-17 20:01:17 -0700

    add theme (dark mode) toggle with actual functionality

---
+++ src/components/Navbar/DarkModeToggle/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
+
+const DarkModeToggle: React.FC = () => {
+    const [isDark, setIsDark] = useState(true);
+
+    // Function to handle the dark mode toggle and set the theme for the whole app
+    const handleThemeToggle = (isDark: boolean) => {
+        setIsDark(!isDark);
+        const root = document.documentElement;
+        const theme = isDark ? 'light' : 'dark';
+        root.classList.remove(isDark ? 'dark' : 'light');
+        root.classList.add(theme);
+        localStorage.setItem('color-theme', theme);
+    };
+
+    return (
+        <button
+            id="theme-toggle"
+            type="button"
+            title="Enable dark mode"
+            aria-label="dark mode"
+            onClick={() => handleThemeToggle(isDark)}
+        >
+            <FontAwesomeIcon icon={faCircleHalfStroke} size="2x" />
+        </button>
+    )
+};
+
+export default DarkModeToggle;
