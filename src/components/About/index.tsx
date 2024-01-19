d6f8ffcfe069c2a9d55600794f18dc03c1f38ed6
Wes Warriner <fridgehistory@gmail.com>
2024-01-18 21:49:49 -0700

    convert styles to taliwind

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,47 +5,47 @@ import logoJs from '../../assets/images/logo_js.svg';
 import logoReact from '../../assets/images/logo_reactjs.svg';
 import logoVue from '../../assets/images/logo_vuejs.svg';
 import logoCpp from '../../assets/images/logo_c_plus_plus.svg';
-import './index.scss';
+
 import Pulse from './Pulse';
 import SequentialShow from './SequentialShow';
 
-const About = () => {
+export default function About() {
     return (
-        <div className="container about-page">
-            <div className="hero">
-                <h1>About Me</h1>
-                <p>
-                    I'm a self-taught developer with a background in data science.
-                </p>
-                <p>
-                    Curiosity is my driving force (also coffee <span role="img" aria-label="coffee mug">☕</span> ngl).
-                </p>
+        <div className="mx-2 my-32">
+            <div className="flex flex-col text-center">
+                <div className="my-4 md:w-[50%] md:text-left">
+                    <h1 className="text-[4rem] mb-2">About Me</h1>
+                    <p>
+                        I'm a full-stack engineer with a background in data science.
+                    </p>
+                    <p>
+                        Curiosity is my driving force (also coffee <span role="img" aria-label="coffee mug">☕</span>).
+                    </p>
+                </div>
+                <SequentialShow delay={1000}>
+                    <Pulse delay={1000} sequentialDelay={1000} >
+                        <img src={logoPython} alt="Python logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={2000} >
+                        <img src={logoGolang} alt="Golang logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={3000} >
+                        <img src={logoLinux} alt="Linux logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={4000} >
+                        <img src={logoJs} alt="Javascript logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={5000} >
+                        <img src={logoReact} alt="React logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={6000} >
+                        <img src={logoVue} alt="Vue logo" />
+                    </Pulse>
+                    <Pulse delay={1000} sequentialDelay={6000} >
+                        <img src={logoCpp} alt="C++ logo" />
+                    </Pulse>
+                </SequentialShow>
             </div>
-            <SequentialShow delay={1000}>
-                <Pulse delay={1000} sequentialDelay={1000} >
-                    <img src={logoPython} alt="Python logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={2000} >
-                    <img src={logoGolang} alt="Golang logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={3000} >
-                    <img src={logoLinux} alt="Linux logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={4000} >
-                    <img src={logoJs} alt="Javascript logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={5000} >
-                    <img src={logoReact} alt="React logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={6000} >
-                    <img src={logoVue} alt="Vue logo" />
-                </Pulse>
-                <Pulse delay={1000} sequentialDelay={6000} >
-                    <img src={logoCpp} alt="C++ logo" />
-                </Pulse>
-            </SequentialShow>
         </div>
     )
 }
-
-export default About;
