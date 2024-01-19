415f5c4dedbbfc33d26d7c26b31843892eb0ea70
Wes Warriner <fridgehistory@gmail.com>
2024-01-19 09:56:02 -0700

    improve About page text, sizing, and spacing

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,21 +12,21 @@ import '../../index.css';
 
 export default function About() {
     return (
-        <div className="mx-2 my-32">
-            <div className="flex flex-col md:flex-row">
-                <div className="my-4">
+        <div className="mx-2 my-32 md:my-96">
+            <div className="flex flex-col md:flex-row justify-center gap-4">
+                <div className="md:w-[33%] mx-24 my-4">
                     <div className="text-center md:text-left">
                         <h1 className="text-[4rem] mb-2">About Me</h1>
-                        <p>
-                            I'm a full-stack engineer with a background in data science.
+                        <p className="text-[1.5rem]">
+                            I'm a full-stack developer with a background in data science.
                         </p>
-                        <p>
+                        <p className="text-[1.5rem]">
                             Curiosity is my driving force (also coffee <span role="img" aria-label="coffee mug">☕</span>).
                         </p>
                     </div>
                 </div>
-                <div className="my-4">
-                    <SequentialShow delay={1000}>
+                <div className="md:w-[33%] mx-24 my-4">
+                    <SequentialShow>
                         <Pulse delay={1000} sequentialDelay={1000} >
                             <img className="w-16" src={logoPython} alt="Python logo" />
                         </Pulse>
@@ -45,7 +45,7 @@ export default function About() {
                         <Pulse delay={1000} sequentialDelay={6000} >
                             <img className="w-16" src={logoVue} alt="Vue logo" />
                         </Pulse>
-                        <Pulse delay={1000} sequentialDelay={6000} >
+                        <Pulse delay={1000} sequentialDelay={7000} >
                             <img className="w-16" src={logoCpp} alt="C++ logo" />
                         </Pulse>
                     </SequentialShow>
