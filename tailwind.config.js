643b02cca7af5308d44ecf35dea08621b45e68ff
Wes Warriner <fridgehistory@gmail.com>
2024-01-20 12:35:06 -0700

    begin adding in palette css vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
       'console': ['Lucida Console', 'Courier New', 'monospace'],
     },
     extend: {
+      colors: {
+        "primary": "var(--primary)",
+        "secondary": "var(--secondary)",
+        "accent-1": "var(--accent-1)",
+        "accent-2": "var(--accent-2)",
+        "accent-3": "var(--accent-3)",
+      },
       gridTemplateColumns: {
         '14': 'repeat(14, minmax(0, 1fr))',
       },
