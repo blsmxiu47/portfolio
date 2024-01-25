c483c428106bf5614a36e9f0fcefbe960756532a
Wes Warriner <fridgehistory@gmail.com>
2024-01-24 19:28:02 -0700

    begin adding dark mode colors, starting with app bg

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+
 import Navbar from '../Navbar';
 
 export default function Layout() {
-    return <>
-        <Navbar />
-        <Outlet />
-    </>
+    return (
+        <main className="bg-[var(--bg)] dark:bg-[var(--bg-dark)] min-h-screen">
+            <Navbar />
+            <Outlet />
+        </main>
+    )
 };
