2fabe69772d2ab9c25520afecd29bac00548399a
Wes Warriner <fridgehistory@gmail.com>
2024-01-26 13:27:22 -0700

    set fixed height for project cards regardless of length of description

--- src/components/Projects/ProjectCard/index.tsx
+++ src/components/Projects/ProjectCard/index.tsx
@@ -9,7 +9,7 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
     return (
         <a
-            className="relative group max-w-[16rem] justify-self-center flex flex-col justify-center mx-2 my-1 rounded-lg shadow-md hover:scale-110 transition duration-700 ease-in-out cursor:pointer bg-gray-100 dark:bg-gray-800"
+            className="relative group h-80 max-w-[16rem] justify-self-center flex flex-col justify-top mx-2 my-1 rounded-lg shadow-md hover:scale-110 transition duration-700 ease-in-out cursor:pointer bg-gray-100 dark:bg-gray-800"
             href={githubLink}
             target="_blank"
             rel="noopener noreferrer"
@@ -28,13 +28,15 @@ const ProjectCard = ({
                 <rect x="-180" y="130" width="8" height="180" transform="rotate(-45)" fill="white" />
             </svg>
             <img className="rounded-t-lg" src={imageSrc} alt="Project demo screenshot" />
-            <div className="px-4 pt-2 pb-4 bg-transparent">
-                <h2 className="pt-2 pb-1 text-[1rem] font-bold text-[var(--primary)] dark:text-white">
-                    {title}
-                </h2>
-                <p className="py-1 text-[0.75rem]">
-                    {summary}
-                </p>
+            <div className="px-4 pt-2 pb-4 bg-transparent flex-1 flex flex-col justify-between">
+                <div>
+                    <h2 className="pt-2 pb-1 text-[1rem] font-bold text-[var(--primary)] dark:text-white">
+                        {title}
+                    </h2>
+                    <p className="py-1 text-[0.75rem]">
+                        {summary}
+                    </p>
+                </div>
                 <div className="flex flex-wrap justify-center gap-4 py-2 w-full">
                     {tags.map((tag, index) => (
                         <span key={index} className="text-[0.75rem] font-semibold text-[var(--primary)] dark:text-white">
