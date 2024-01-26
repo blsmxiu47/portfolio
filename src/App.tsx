95ffb51cdbe0431cdc4ab368cf1893f5bd720f29
Wes Warriner <fridgehistory@gmail.com>
2024-01-25 19:02:41 -0700

    wrap app in context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+
+import { NavMenuContextProvider } from './contexts/NavMenuContext'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import Projects from './components/Projects'
@@ -7,12 +9,14 @@ import About from './components/About'
 export default function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="about" element={<About />} />
-      </Route>
-    </Routes>
+    <NavMenuContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </NavMenuContextProvider>
   )
 }
