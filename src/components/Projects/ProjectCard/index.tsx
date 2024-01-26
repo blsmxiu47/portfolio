6ab7c46901bbe45daf7c3b73c49090641c6db2d1
Wes Warriner <fridgehistory@gmail.com>
2024-01-25 18:05:15 -0700

    adjust project cards image size, bg color

--- src/components/Projects/ProjectCard/index.tsx
+++ src/components/Projects/ProjectCard/index.tsx
@@ -8,9 +8,27 @@ const ProjectCard = ({
     githubLink
 }: ProjectCardProps) => {
     return (
-        <a className="max-w-[16rem] justify-self-center flex flex-col justify-center mx-2 my-1 rounded-lg shadow-md hover:scale-110 transition duration-700 ease-in-out cursor:pointer" href={githubLink} target="_blank" rel="noopener noreferrer">
+        <a
+            className="relative group max-w-[16rem] justify-self-center flex flex-col justify-center mx-2 my-1 rounded-lg shadow-md hover:scale-110 transition duration-700 ease-in-out cursor:pointer bg-gray-100"
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <svg
+                className="absolute z-[-5] bottom-0 left-0"
+                viewBox="0 0 375 283"
+                fill="none"
+                style={{ opacity: 0.4 }}
+            >
+                <rect x="180" y="170" width="8" height="180" transform="rotate(-45)" fill="white" />
+                <rect x="200" y="170" width="8" height="180" transform="rotate(-45)" fill="white" />
+                <rect x="220" y="170" width="8" height="180" transform="rotate(-45)" fill="white" />
+                <rect x="-140" y="130" width="8" height="180" transform="rotate(-45)" fill="white" />
+                <rect x="-160" y="130" width="8" height="180" transform="rotate(-45)" fill="white" />
+                <rect x="-180" y="130" width="8" height="180" transform="rotate(-45)" fill="white" />
+            </svg>
             <img className="rounded-t-lg" src={imageSrc} alt="Project demo screenshot" />
-            <div className="px-4 pt-2 pb-4">
+            <div className="px-4 pt-2 pb-4 bg-transparent">
                 <h2 className="pt-2 pb-1 text-[1rem] font-bold text-[var(--primary)]">
                     {title}
                 </h2>
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -9,56 +9,56 @@ export default function Projects() {
                     <ProjectCard
                         title="Fearless Fitness Tracker"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["React", "TypeScript", "Tailwind CSS"]}
                         githubLink="https://github.com/blsmxiu47/fearless-fitness-tracker"
                     />
                     <ProjectCard
                         title="Pace Converter"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["React", "TypeScript", "Tailwind CSS"]}
                         githubLink="https://github.com/blsmxiu47/pace-converter"
                     />
                     <ProjectCard
                         title="Fantastic Product"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["React", "TypeScript", "Tailwind CSS"]}
                         githubLink="https://github.com/blsmxiu47/fantastic-product"
                     />
                     <ProjectCard
                         title="Time Since..."
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["React", "TypeScript", "Tailwind CSS"]}
                         githubLink="https://github.com/blsmxiu47/time-since"
                     />
                     <ProjectCard
                         title="cIRCb"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["C"]}
                         githubLink="https://github.com/blsmxiu47/circb"
                     />
                     <ProjectCard
                         title="Haebaragi"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["Golang"]}
                         githubLink="https://github.com/blsmxiu47/haebaragi"
                     />
                     <ProjectCard
                         title="Gorter"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["Golang"]}
                         githubLink="https://github.com/blsmxiu47/gorter"
                     />
                     <ProjectCard
                         title="Glaso"
                         summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae aliquam nisl."
-                        imageSrc="https://via.placeholder.com/200x150"
+                        imageSrc="https://via.placeholder.com/200x100"
                         tags={["JavaScript", "WebExtensions API"]}
                         githubLink="https://github.com/blsmxiu47/glaso"
                     />
