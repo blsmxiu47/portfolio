1abc51fe805b8af0a6d99c47901c39f9c547d06d
Wes Warriner <fridgehistory@gmail.com>
2024-02-20 10:35:11 -0700

    handle initial dark/light theme setting in index.html to avoid FOUC; simplify DarkModeToggle logic

--- src/components/Navbar/DarkModeToggle/index.tsx
+++ src/components/Navbar/DarkModeToggle/index.tsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
 
 import '../../../index.css';
 
 export default function DarkModeToggle() {
-    const [colorTheme, setTheme] = useState(localStorage.getItem('color-theme'));
+    const [colorTheme, setTheme] = useState(document.documentElement.classList.contains('dark') ? 'dark' : 'light');
     const [isDark, setIsDark] = useState(colorTheme === 'dark');
 
-    const handleThemeToggle = () => {
-        setTheme(colorTheme);
-        setIsDark(!isDark);
+    useEffect(() => {
+        setTheme(isDark ? 'dark' : 'light');
         const root = document.documentElement;
         const theme = isDark ? 'dark' : 'light';
         root.classList.remove(isDark ? 'light' : 'dark');
         root.classList.add(theme);
-        localStorage.setItem('color-theme', theme);
-    };
+        localStorage.setItem('theme', theme);
+    }, [isDark]);
 
     return (
         <button
@@ -24,7 +23,7 @@ export default function DarkModeToggle() {
             type="button"
             title="Enable dark mode"
             aria-label="dark mode"
-            onClick={handleThemeToggle}
+            onClick={() => setIsDark(!isDark)}
         >
             <FontAwesomeIcon className="size-6 text-[var(--primary)] dark:text-[var(--primary-dark)] hover:text-[var(--secondary)] dark:hover:text-[var(--secondary)]" icon={faCircleHalfStroke} />
         </button>
